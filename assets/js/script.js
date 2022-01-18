80f79e57031b311e0a7df0ff5f6b654146845e74
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-18 19:53:18 +0000

    start timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,61 @@
-//required elements
-const start_btn = document.querySelector(".start_btn button");
-const rules_box = document.querySelector(".rules_box");
-const quit = document.querySelector(".buttons .quit");
-const restart = document.querySelector(".buttons .restart");
+//timer start
+var timeLeft = 75;
+var timerID;
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-quiz-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerEl = document.getElementById("question-container");
+var startContainerEl = document.getElementById("start-container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answer-buttons");
+var checkAnswerEl = document.getElementById("check-answer");
+var viewHighScores = document.getElementById("highscores-link");
+var submitButton = document.getElementById("submit-btn");
+var clearScoreButton = document.getElementById("clear-btn");
+var initialsField = document.getElementById("player-name");
+var restartButton = document.getElementById("restart-btn");
+var scoreField = document.getElementById("player-score");
+var scores = JSON.parse(localStorage.getItem("scores")) || [];
 
-//start button clicked 
-start_btn.onClick = {
-    rules_box.classList.add ("activeInfo");
+var shuffledQuestions, currentQuestionIndex;
+
+//start button
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
+
+
+//timer countdown
+function timeTick() {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+        saveScore();
+    }
 }
+
+//start quiz
+
+function startGame() {
+    timerID = setInterval(timeTick, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random () - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide");
+
+    timeTick();
+    setNextQuestion();
+};
+
+//Next question
+function setNextQuestion() {
+    resetState ();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+//Show Questions
+
+
   
