37d4117cdb3d18f1486b751d3d6425c7236a226f
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-18 16:47:37 +0000

    adding questions

---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+//questions for the quiz
+var questions = [
+     {
+         question: "At the first meeting of the Duelling Club, Draco Malfoy summoned what animal with the spell ‘Serpensortia’?",
+         answers: [
+             {text: "Frog", correct: false },
+             {text: "Bear", correct: false },
+             {text: "Snake", correct: true },
+             {text: "Dragon", correct: false }
+         ]
+     }
+]
