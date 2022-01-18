cd558294dffc99c2d6a02c2c39e13bcd2ff389be
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-18 15:02:56 +0000

    completed html

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//required elements
+const start_btn = document.querySelector(".start_btn button");
+const rules_box = document.querySelector(".rules_box");
+const quit = document.querySelector(".buttons .quit");
+const restart = document.querySelector(".buttons .restart");
+
+//start button clicked 
+start_btn.onClick = {
+    rules_box.classList.add ("activeInfo");
+}
+  
