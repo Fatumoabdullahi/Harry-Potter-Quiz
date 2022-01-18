82c0c27a72fc35116611ef8fed466cca98469ac5
Fatumoabdullahi <95045817+Fatumoabdullahi@users.noreply.github.com>
2022-01-18 18:50:41 +0000

    added all questions and answer options

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,5 +8,41 @@ var questions = [
              {text: "Snake", correct: true },
              {text: "Dragon", correct: false }
          ]
+     },
+     {
+         question: "The 'Felifors' spell turns a cat into what?",
+         answers: [
+             {text: "Matchbox", correct: false},
+             {text: "Cauldron", correct: true},
+             {text: "Hat", correct: false},
+             {text: "Bat", correct: false},
+         ]
+     }
+     {
+         question: "Which element is associated with Hufflepuff?",
+         answers: [
+             {text:"Earth", correct: true},
+             {text: "Fire", correct: false},
+             {text: "Water", correct: false},
+             {text: "Air", correct: false},
+         ]
+     }
+     {
+         question: "What was the name of the Black family’s house elf?",
+         answers: [
+            {text:"Bobby", correct: false},
+            {text: "Winky", correct: false},
+            {text: "Dobby", correct: true},
+            {text: "Kreacher", correct: false},
+         ] 
+     }
+     {
+         question: "What position does Harry play on his Quidditch team?",
+         answers: [
+             {text:"Chaser", correct: false},
+             {text:"Seeker", correct: true},
+             {text:"Bludger", correct: false},
+             {text:"keeper", correct: false},
+         ]
      }
 ]
